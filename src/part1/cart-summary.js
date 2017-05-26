2375c6dae52902e88e6e13c4829e13952b84378a
ldaher <luciano.daher@gmail.com>
2017-05-26 11:58:00 -0300

    FIRST

--- src/part1/cart-summary.js
+++ src/part1/cart-summary.js
@@ -4,6 +4,8 @@ function CartSummary(items){
     this._items = items;
 }
 
+
+//TODO DELETAR
 CartSummary.prototype.getSubtotal = function() {
     if(this._items.length) {
         return this._items.reduce(function(subtotal, item) {
